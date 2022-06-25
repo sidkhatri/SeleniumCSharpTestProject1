49a781a1a1de39a0b9d10410cbe3137cb96ec18a
Siddique Khatri <sidkhatri@gmail.com>
2022-06-25 13:01:09 -0400

    Added additional XPath

--- SeleniumCSharpTestProject1/UnitTest1.cs
+++ SeleniumCSharpTestProject1/UnitTest1.cs
@@ -23,12 +23,19 @@ namespace SeleniumCSharpTestProject1
             Driver.FindElement(By.Id("ContentPlaceHolder1_Meal")).SendKeys("Tomato");
 
             Driver.FindElement(By.XPath("//input[@name='ctl00$ContentPlaceHolder1$ChildMeal1']/following-sibling::div[text()='Celery']")).Click();
-           
+
+            IWebElement comboControl = Driver.FindElement(By.XPath("//input[@id='ContentPlaceHolder1_AllMealsCombo-awed']"));
+
+            comboControl.Clear();
+            comboControl.SendKeys("Almond");
+            IWebElement dropDropDownSelect = Driver.FindElement(By.XPath("//body/div[@id='ContentPlaceHolder1_AllMealsCombo-dropmenu']/div[2]/ul[1]/li[1]"));
+
+            dropDropDownSelect.Click(); 
             
             Console.WriteLine("Test1");
             Assert.Pass();
-            Driver.Quit();
             Driver.Close();
+            Driver.Quit(); 
         }
     }
 }
